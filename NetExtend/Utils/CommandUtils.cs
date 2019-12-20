faa2e8c170dcf14ed83b77c8c7be7cbc57071bc4
FrankTsui <Frank_Tsai@dynamicpcb.com.tw>
2019-12-20 10:27:39 +0800

    Add CommandUtils

---
+++ NetExtend/Utils/CommandUtils.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetExtend.Utils
+{
+    public class CommandUtils
+    {
+        public static void ExecuteCommandSync(object command)
+        {
+            try
+            {                
+                ProcessStartInfo procStartInfo = new ProcessStartInfo("cmd", "/c " + command);                
+                procStartInfo.RedirectStandardOutput = true;
+                procStartInfo.UseShellExecute = false;                
+                procStartInfo.CreateNoWindow = true;                
+                Process proc = new Process();
+                proc.StartInfo = procStartInfo;
+                proc.Start();                
+                string result = proc.StandardOutput.ReadToEnd();                
+                Console.WriteLine(result);
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
